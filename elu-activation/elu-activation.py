98e08893c66c5622003886c72946f9800fffa9d8
Dylan Ng <124280476+mnDylan@users.noreply.github.com>
2026-03-07 14:19:05 +0700

    Update solution for elu-activation

--- elu-activation/elu-activation.py
+++ elu-activation/elu-activation.py
@@ -4,15 +4,13 @@ def elu(x, alpha):
     """
     Apply ELU activation to each element.
     """
-
-    len_x = len(x)
     result = []
-    for i in range(len_x): 
-        if x[i] > 0: 
-            result_activate = x[i] 
+    for xi in x: 
+        if xi > 0: 
+            result_activate = xi
             result.append(result_activate)
         elif alpha >= 0: 
-            result_activate = alpha * (math.exp(x[i]) -1)
+            result_activate = alpha * (math.exp(xi) -1)
             result.append(result_activate)
         else:
             print('Please input valit alpha >= 0 ')
